77822fed3822b9b308be247ada2469769f025b3d
au2006del <au2006del@gmail.com>
2017-10-03 17:41:13 -0400

    first commit

---
+++ sendsmstwilioAnshul.php
@@ -0,0 +1,24 @@
+<?php
+// this line loads the library
+// require('Twilio.php');
+// use vendor\twilio\sdk\Twilio\Rest\Client;
+error_reporting(E_ALL);
+require_once "vendor/autoload.php";
+use Twilio\Rest\Client;
+echo "Hello";
+
+$account_sid = 'ACa49266a70a3af56e47fcc14864334c3b' ;
+$auth_token = 'c96173043eb7864c8eaa676ac4e597be';
+$client = new Client($account_sid, $auth_token);
+
+
+
+
+$client->messages->create(
+'+15512295673',
+array(
+// 'To' => "+15512295673",
+'From' => "+16283003802",
+'Body' => "Hello Anshul Upadhyay CS643 Fall 2017"
+));
+?>
